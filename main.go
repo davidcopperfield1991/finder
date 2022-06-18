5a9d1db88e5139636385f3a00d2d256e4369d7a7
davidcopperfield1991 <david.moh1991@gmail.com>
2022-06-18 15:01:32 +0430

    v1

---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// const INPUT_FILE = "value.txt"
+const INPUT_FILE = "../../.bash_history"
+
+func readLines(path string) ([]string, error) {
+
+	_, err := os.ReadFile(INPUT_FILE)
+	if err != nil {
+		fmt.Println("file dastan shod")
+	}
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func peydakon(loghat string) []string {
+	arzesh, _ := readLines(INPUT_FILE)
+
+	sort.Strings(arzesh)
+
+	// var p string
+
+	fmt.Scanln(loghat)
+
+	fmt.Println(loghat)
+	var javab []string
+	for i := range arzesh {
+		s := strings.Contains(arzesh[i], loghat)
+
+		if s == true {
+			fmt.Println(arzesh[i])
+			javab = append(javab, arzesh[i])
+		}
+	}
+	fmt.Println("javab")
+	return javab
+}
+
+type UU struct {
+	Matn map[string]interface{}
+}
+
+func find(c *fiber.Ctx) error {
+	loghat := c.Params("loghat")
+	peydakon(loghat)
+	fmt.Println(loghat)
+	// return nil
+	// var belakhare UU
+	ann := peydakon(loghat)
+	// fmt.Println(ann)
+	return c.JSON(ann)
+}
+
+func Routers(app *fiber.App) {
+	app.Get("/find/:loghat", find)
+}
+
+func main() {
+	// peydakon()
+	app := fiber.New()
+	Routers(app)
+
+	app.Listen(":3333")
+
+}
